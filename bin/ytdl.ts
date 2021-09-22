0bec9312efbd2d5681dae199d3a52168365f1a2d
palmtrww69 <palmsaico@gmail.com>
2021-09-22 16:35:07 -0700

    Code

---
+++ bin/ytdl.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const path = require("path")
+const envFile = path.join(process.env.HOME, "env.json")
+const Fs = require("fs")
+
+if (!Fs.existsSync(envFile)) Fs.writeFileSync(envFile, JSON.stringify({ baseDir: "downloads" }))
+
+const { start, retry } = require("../src/index")
+
+if (process.argv.length === 4 && process.argv[2] === "--setDir") {
+    const config = require(envFile)
+    config.baseDir = process.argv[3]
+    Fs.writeFileSync(envFile, JSON.stringify(config))
+} else if (process.argv.length === 3 && process.argv[2] === "--retry") {
+    retry()
+} else {
+    start()
+}
